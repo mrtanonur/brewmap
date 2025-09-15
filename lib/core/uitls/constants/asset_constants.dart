18769128e02c5c9521558bf0adbae1d64c610a73
mrtanonur <onurtan@gmail.com>
2025-09-15 20:03:07 +0300

    feat(constants): add assets, color, size constants

---
+++ lib/core/uitls/constants/asset_constants.dart
@@ -0,0 +1,7 @@
+class AssetConstants {
+  static const String appleLogo = "assets/images/apple_logo.png";
+  static const String brewmapTransparent =
+      "assets/images/brewmap_transparent.png";
+  static const String brewmap = "assets/images/brewmap.png";
+  static const String googleLogo = "assets/images/google_logo.png";
+}
